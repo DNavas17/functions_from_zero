78a40af16356305774472b850e1d76dd384df12d
DNavas17 <150387219+DNavas17@users.noreply.github.com>
2023-11-15 16:55:12 +0000

    fixing issues

--- wikibot.py
+++ wikibot.py
@@ -1,14 +1,16 @@
 from mylib.bot import scrape
 import click
 
+
 @click.command()
-@click.option('--name', prompt='Wkipedia page to scrape', help='Web page we want to scrape')
-
-@click.option('--length', help="lenght of the output from wikipedia")
-
+@click.option(
+    "--name", prompt="Wkipedia page to scrape", help="Web page we want to scrape"
+)
+@click.option("--length", help="lenght of the output from wikipedia")
 def cli(name, length):
-    result = scrape(name, length=length)
+    result = scrape(name, length)
     click.echo(click.style(f"{result}:", bg="black", fg="blue"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
